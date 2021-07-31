e035d3e16fc97872af31a4d9aa367762ab264aaf
Rupinder <rupinder.developer@gmail.com>
2021-08-01 00:14:39 +0530

    Added AsyncLoop

--- index.js
+++ index.js
@@ -1,6 +1,116 @@
+/**
+ * Async Loop is used to iterate over an given array or range without blocking 
+ * event loop.
+ * 
+ * ******************
+ * EXAMPLE 1 (Array):
+ * ******************
+ * 
+ * const array = new AsyncLoop([1, 2, 3, 4]);
+ * 
+ * array.each((value, index, array) => {
+ *      console.log(value, index, array);
+ * }).then(() => {
+ *      // After completion
+ * }).catch(() => {
+ *      // Something went wrong!!
+ * });
+ * 
+ * ******************
+ * EXAMPLE 2 (Range):
+ * *****************
+ * 
+ * const range = new AsyncLoop(0, 10);
+ * 
+ * range.each(index => {
+ *      console.log(index);
+ * }).then(() => {
+ *      // After completion
+ * }).catch(() => {
+ *      // Something went wrong!!
+ * });
+ */
 class AsyncLoop {
-    constructor() {
-        
+    /**
+     * AsyncLoop()
+     * 
+     * @param {array | number} x 
+     * @param {number} y 
+     */
+    constructor(x, y = null) {
+        this._range = null;
+        this._array = null;
+
+        if (typeof x == 'number') {
+            // Set Range =>
+            if (y != null && typeof y == 'number') {
+                if (x <= y) {
+                    this._range = {
+                        from: x, 
+                        to: y
+                    }
+                }
+            } else {
+                this._range = {
+                    from: 0,
+                    to: x
+                }
+            }
+        } else if (Array.isArray(x)) {
+            // Set Array => 
+            this._array = x;
+        }
+    }
+
+    /**
+     * each() is used to iterate over an given array of range.
+     * 
+     * @param {function} cb 
+     * @returns {Promise}
+     */
+    each(cb) {
+        return new Promise((resolve, reject) => {
+            this._recursion(cb, resolve, reject);
+        });
+    }
+
+    /**
+     * _recursion() is used to asychronous call the callback (cb) function.
+     * 
+     * @param {function} cb 
+     * @param {function} resolve 
+     * @param {function} reject 
+     * @param {number} index  
+     */
+    async _recursion(cb, resolve, reject, index = 0) {
+        if (this._array != null) {
+            if (this._array.length == index) {
+                resolve();
+            } else {
+                cb(this._array[index], index, this._array);
+
+                setImmediate(() => {
+                    this._recursion(cb, resolve, reject, ++index);
+                });
+            }
+        } else if (this._range != null) {
+            const { from, to } = this._range;
+            if (index < from) {
+                index = from;
+            } 
+            
+            if (index == to) {
+                resolve();
+            } else {
+                cb(index);
+
+                setImmediate(() => {
+                    this._recursion(cb, resolve, reject, ++index);
+                });
+            }
+        } else {
+            reject();
+        }
     }
 }
 
